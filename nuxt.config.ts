7d1a8e6f3347b0ef317810d04ebb4a3a7587be10
enotus-chang <anton.s.chegus@gmail.com>
2025-11-08 15:00:40 +0500

    deploy workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
+  ssr: false,
+  app: {
+    baseURL: '/c0d3rs_d3n/'
+  },
+  nitro: {
+    preset: 'github-pages'
+  },
+  devtools: {
+    enabled: true
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
