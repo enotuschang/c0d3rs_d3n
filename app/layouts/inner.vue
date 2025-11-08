a6886850195d413b2c90345dda3ea59a2d1045b2
enotus-chang <anton.s.chegus@gmail.com>
2025-11-08 16:54:31 +0500

    Add About

---
+++ app/layouts/inner.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Header/>
+
+  <main>
+    <NuxtLink to="/">Go back home</NuxtLink>
+
+    <slot/>
+  </main>
+
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
